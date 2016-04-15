50ce01f939e17a2ad9b5e226b0eb16821ad3f7ac
daiheidoi <dd11197208+cloud9@gmail.com>
2016-04-15 02:00:50 +0000

    sh stop start

---
+++ shellscript/start_worker.sh
@@ -0,0 +1,6 @@
+#/bin/sh
+
+/usr/local/bin/curl -n -X PATCH https://api.heroku.com/apps/linebot-hubot/formation/worker \
+-H "Accept: application/vnd.heroku+json; version=3" \
+-H "Content-Type: application/json" \
+-d '{ "quantity": 1 }'
